7dd23e36fa1748fd6f6871e5c09587db84472e8c
Mazhar Ahmad Nomani <mazharahmadnomani@gmail.com>
2024-08-24 23:24:04 +0530

    streamlit_deleted

--- streamlit.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import streamlit as st
-
-# Title
-st.title("Mazhar Bhai")
-name = st.text_input("Your Name", placeholder="Type your name here...")
-st.write(f"your name is {name}")
